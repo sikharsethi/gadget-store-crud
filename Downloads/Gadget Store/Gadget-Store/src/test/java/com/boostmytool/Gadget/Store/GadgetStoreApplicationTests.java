0052d236b2af88b5779b896eeb7527f709419542
Sikhar Sethi <sikharsethi5@gmail.com>
2026-03-16 23:40:32 +0530

    initial commit

---
+++ Downloads/Gadget Store/Gadget-Store/src/test/java/com/boostmytool/Gadget/Store/GadgetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boostmytool.Gadget.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
